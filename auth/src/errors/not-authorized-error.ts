8969c3c4d98f2b864d91de37fca779ea876f429a
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-11-10 15:51:46 +0100

    require auth for route access

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,15 @@
+import { updateRestTypeNode } from "typescript";
+import { CustomError } from "./custom-error";
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super("Not authorized");
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: "Not authorized" }];
+  }
+}
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 router.get(
   "/api/users/currentuser",
   currentUser,
+  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
   }
