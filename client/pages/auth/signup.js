d98e12bdbfc335731dfec639ec2db5d542f43a61
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-11-12 15:50:32 +0100

    handle validation errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(email, password);
-    setEmail("");
-    setPassword("");
+
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
   return (
     <form onSubmit={onSubmit}>
@@ -22,14 +31,24 @@ export default () => {
         />
       </div>
       <div classname="form-group">
-        <label>Password</label>
+        <label className="mt-3">Password</label>
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           type="password"
-          className="form-control"
+          className="form-control mb-3"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger mt-3">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
