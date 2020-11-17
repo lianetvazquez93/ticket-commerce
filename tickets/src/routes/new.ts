1099af2de4c41280e7932d9d4d3ad84e935d7491
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-11-17 12:04:34 +0100

    validate title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@lv93tickets/common";
+import { body } from "express-validator";
+import { requireAuth, requestValidator } from "@lv93tickets/common";
 
 const router = express.Router();
 
 router.post(
   "/api/tickets",
   requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  requestValidator,
   async (req: Request, res: Response) => {
     res.sendStatus(200);
   }
