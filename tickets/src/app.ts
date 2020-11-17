d70206a424850c2363f4fcb382911fb427703ba2
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-11-17 17:52:20 +0100

    test update ticket route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import { errorHandler, NotFoundError, currentUser } from "@lv93tickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { fetchTicketsRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -23,6 +24,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(fetchTicketsRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -42,7 +42,7 @@ afterAll(async () => {
 
 global.signup = () => {
   const payload = {
-    id: "bvcgdf4we3",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
